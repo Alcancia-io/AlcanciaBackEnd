1bd3eb45bcdd12f2934d4266f873eee1fe91ffcf
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2021-10-20 18:25:28 -0500

    create paypal order

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js';
 import dotenv from "dotenv";
 import cors from 'cors';
+import createOrder from './paypalClient.js';
 
 dotenv.config();
 const app = express();
@@ -19,6 +20,21 @@ app.use('/users',usersRoutes);
 //routes
 app.get('/',(req,res) => {
     res.send('Hellow world');
+});
+
+app.get('/create-order',async (req,res) =>{
+    var result=await createOrder();
+    //console.log(result);
+    res.send(result);
+});
+
+app.get('/execute-order',(req,res)=>{
+    console.log(req.query);
+    res.send('hi');
+});
+
+app.get('/cancel-order',(req,res)=>{
+    res.send('cacelled');
 });
 
 app.listen(process.env.PORT,() => {
--- paypalClient.js
+++ paypalClient.js
@@ -27,8 +27,64 @@ var getPaypalToken = async function (){
   }
 }
 
+var requestOrder = async function (body,token){
+    
+}
+
 var token=await getPaypalToken();
-console.log(token);
+//console.log(token);
 
 
+async function createOrder(){
+  const body = {
+    intent: 'CAPTURE',
+    purchase_units: [{
+      amount: {
+        currency_code: 'USD',
+        value: '100'
+      }
+    }],
+    application_context: {
+      brand_name: 'Alcancia',
+      landing_page: 'NO_PREFERENCE',
+      user_action: 'PAY_NOW',
+      return_url: 'http://localhost:8000/execute-order',
+      cancel_url: 'http://localhost:8000/cancel-order'
+    }
+  };
+
+  let result;
+  try{
+    await axios({
+      url: process.env.Paypal+'/v2/checkout/orders',
+      port: 443,
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      data: body
+      //json: true
+    })
+    .then(function (response){
+      //console.log(response.data.links);
+      for(let i=0;i<response.data.links.length;i++){
+        if(response.data.links[i].rel=='approve'){
+          //console.log(response.data.links[i].href);
+          result= response.data.links[i].href;
+          //return 'sadfgds';
+        }
+      }
+      
+    });
+  }catch(e){
+    console.error(e);
+  }
+  //requestOrder(body,token);  
+  return result;
+}
+
+//var order= await createOrder();
+export default createOrder;
 
