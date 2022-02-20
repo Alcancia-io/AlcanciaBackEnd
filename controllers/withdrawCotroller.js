97d186aea17eb8cf82fa4d83e3a2f365c8f26951
Royer Donnet Arenas Camacho <A01209400@itesm.mx>
2022-02-19 18:29:34 -0600

    wip withdraw operation

--- controllers/withdrawCotroller.js
+++ controllers/withdrawCotroller.js
@@ -1,10 +1,23 @@
 const { firestore } = require('firebase-admin');
 
-module.exports = class WithdrawCotroller{
+module.exports = class WithdrawController{
     
-    static async doWithdraw(){
+    static async doWithdraw(uid,amount,country,bankAccount){
+        switch(country){
+            case 'MEX':
+                break;
+            case 'DOM':
+                break;
+        }
         //check current balance
+        user = await firestore().collection('users').doc(uid).get();
+        
+        if(amount>user.balance){
+            return -1;
+        }
         //create pending trans
+        //missing id for withdraw op
+        await firestore.collection('users').doc('users').doc(uid).collection('withdraws').doc('');
         //subs amount from balance
     }
 }
--- routes/withdraws.js
+++ routes/withdraws.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {param,header,body,validationResult} = require('express-validator');
 const {checkAuth} = require('../middlewares/firebase.js');
+const withdrawController = require('../controllers/withdrawCotroller.js');
 
 router.post('/',
-            checkAuth,
+            //checkAuth,
             withdrawController.doWithdraw
 );
 
